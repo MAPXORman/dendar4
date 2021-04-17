2159f24f7dc21a39e419a2e33cf04ffab4fe7272
MAPXORman <82373394+MAPXORman@users.noreply.github.com>
2021-04-17 14:32:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('Nzg0MzAzNzU2OTI1NDY4NzEz.X8nVyw.zpb11bAyFrQgh6B_INsVs1S38SY');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
